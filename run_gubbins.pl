c62a52d5d2e9d95b1deaccf471a18914c5221853
andrewjpage <andrewjpage@gmail.com>
2011-11-30 11:16:14 +0000

    script to bring it all together

---
+++ run_gubbins.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+
+=head1 NAME
+=head1 SYNOPSIS
+=head1 DESCRIPTION
+=head1 CONTACT
+=head1 METHODS
+ 
+=cut
+
+use strict;
+use warnings;
+
+raxmlHPC -f d  -m GTRGAMMA -n parisomony.tree.phylip.ra5 -t RAxML_result.sl.FINAL.aln.phylip.ra 

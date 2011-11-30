a4662d290caec849772a8ef6d9fc8fd14d92a51a
andrewjpage <andrewjpage@gmail.com>
2011-11-30 12:02:52 +0000

    better help text in main

--- main.c
+++ main.c
@@ -27,16 +27,11 @@
 
 static void print_usage()
 {
-	puts("Find SNP sites");
-	puts("./multi_fasta_to_vcf -s file.aln ");
-	puts("Run Gubbins");
-	puts("./multi_fasta_to_vcf -r file.aln file.aln.vcf file.aln.vcf.tre file.aln.phylip");
-	puts("");
-	puts("Input:");
-	puts("\tfile.aln\t:file containting a multi fasta alignment");
-	puts("");
-	puts("Output:");
-	puts("\tfile.aln.vcf\t:VCF file containing all SNP sites");
+	printf("First step: Find SNP sites\n\n");
+	printf("./gubbins -s file.aln\n\n");
+	printf("Run your favourite tree building program like RaXML\n");
+	printf("Iteratively run Gubbins\n\n");
+	printf("./gubbins -r file.aln file.aln.vcf file.aln.vcf.tre file.aln.phylip\n");
 }
 
 
@@ -53,17 +48,18 @@ int main (int argc, const char * argv[]) {
 	char tree_filename[MAX_FILENAME_SIZE];
 	char phylip_filename[MAX_FILENAME_SIZE];
 
-	
-	if(strcmp(argv[1], "--help") == 0)
+  if(argc <=1)
+  {
+    print_usage();
+  }
+	else if(strcmp(argv[1], "--help") == 0)
     {
 		print_usage();
-		return 0;
     }
 	else if(strcmp(argv[1], "-s") == 0)
     {
 		strcpy(multi_fasta_filename,argv[2]);
 		generate_snp_sites(multi_fasta_filename);
-		return 0;
     }
 	else if(strcmp(argv[1], "-r") == 0)
     {
@@ -73,9 +69,12 @@ int main (int argc, const char * argv[]) {
 		strcpy(phylip_filename,argv[5]);
 		
         run_gubbins(vcf_filename,tree_filename,phylip_filename,multi_fasta_filename);
-		return 0;
     }
-	
+    else
+    {
+      print_usage();
+    }
+    
 	return 0;
 }
 

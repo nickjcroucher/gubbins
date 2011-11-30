f7f7ac61e20321c57d9d68a5a56ba6eae7ed3615
andrewjpage <andrewjpage@gmail.com>
2011-11-30 11:36:54 +0000

    use makefile

--- makefile
+++ makefile
@@ -1,2 +1,43 @@
-all:
-	 gcc -Wall *.c -o multi_fasta_to_vcf
+gubbins: seqUtil.o Newickform.o alignment_file.o fasta_of_snp_sites.o main.o parse_vcf.o snp_searching.c vcf.o branch_sequences.o gubbins.o parse_phylip.o phylib_of_snp_sites.o snp_sites.o
+	gcc -o gubbins seqUtil.o Newickform.o alignment_file.o fasta_of_snp_sites.o main.o parse_vcf.o snp_searching.c vcf.o branch_sequences.o gubbins.o parse_phylip.o phylib_of_snp_sites.o snp_sites.o
+
+seqUtil.o: seqUtil.c
+	gcc -c seqUtil.c
+Newickform.o: Newickform.c
+	gcc -c Newickform.c
+
+alignment_file.o: alignment_file.c
+	gcc -c alignment_file.c
+
+branch_sequences.o: branch_sequences.c
+	gcc -c branch_sequences.c
+
+fasta_of_snp_sites.o: fasta_of_snp_sites.c
+	gcc -c fasta_of_snp_sites.c
+
+gubbins.o: gubbins.c
+	gcc -c gubbins.c
+
+main.o: main.c
+	gcc -c main.c
+
+parse_phylip.o: parse_phylip.c
+	gcc -c parse_phylip.c
+
+parse_vcf.o: 	parse_vcf.c
+	gcc -c parse_vcf.c
+
+phylib_of_snp_sites.o: 	phylib_of_snp_sites.c
+	gcc -c phylib_of_snp_sites.c
+
+snp_searching.o: snp_searching.c
+	gcc -c snp_searching.c
+
+snp_sites.o: snp_sites.c
+	gcc -c snp_sites.c
+
+vcf.o: vcf.c
+	gcc -c vcf.c
+
+clean:
+	-rm *.o
--- run_gubbins.pl
+++ run_gubbins.pl
@@ -1,8 +1,17 @@
 #!/usr/bin/env perl
 
 =head1 NAME
+ 
+ run_gubbins.pl
+ 
 =head1 SYNOPSIS
+ 
+ run_gubbins.pl 
+ 
 =head1 DESCRIPTION
+ 
+ Assumes you have RaXML in your PATH.
+ 
 =head1 CONTACT
 =head1 METHODS
  
@@ -10,5 +19,29 @@
 
 use strict;
 use warnings;
+use File::Basename;
 
-raxmlHPC -f d  -m GTRGAMMA -n parisomony.tree.phylip.ra5 -t RAxML_result.sl.FINAL.aln.phylip.ra 
+my $tree_builder_exec = 'raxmlHPC -f d  -m GTRGAMMA';
+#-n parisomony.tree.phylip.ra5 -t RAxML_result.sl.FINAL.aln.phylip.ra -s parisomony.tree.phylip
+my $number_of_iterations = 5;
+
+my $input_multi_fasta_alignment_file = $ARGV[0];
+
+my($filename, $directories, $suffix) = fileparse($path,  qr/\.[^.]*/);
+
+# Initial step to find SNPs
+system("gubbins -s $ARGV[0]");
+
+for($i = 1; $i <= $number_of_iterations; $i++)
+{
+	my $previous_tree = '';
+	if($i > 1)
+	{
+		$previous_tree  = "-t RAxML_result.iteration_".($i-1);
+	}
+	system("$tree_builder_exec -s $input_multi_fasta_alignment_file.phylip -n iteration_1 $previous_tree");
+	
+	# delete old temp files
+	# run RaXML 
+	system("gubbins -r $input_multi_fasta_alignment_file $input_multi_fasta_alignment_file.vcf RAxML_result.iteration_$i  $input_multi_fasta_alignment_file.phylip");
+}

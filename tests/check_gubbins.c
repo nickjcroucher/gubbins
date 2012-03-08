e0022b3ac916d770dd9a8aa287facf591a5749e9
andrewjpage <andrewjpage@gmail.com>
2012-03-08 13:53:30 +0000

    add some simple tests for recombinations

--- tests/check_gubbins.c
+++ tests/check_gubbins.c
@@ -21,11 +21,59 @@ START_TEST (check_gubbins_produces_files)
 }
 END_TEST
 
+START_TEST (check_gubbins_no_recombinations)
+{
+	run_gubbins("data/no_recombinations.aln.vcf", "data/no_recombinations.aln.tre", "data/no_recombinations.aln.tre","data/no_recombinations.aln.snp_sites.aln");
+	fail_unless(file_exists("data/no_recombinations.tre.tab") == 1);
+	fail_unless(file_exists("data/no_recombinations.tre.vcf") == 1);
+	fail_unless(file_exists("data/no_recombinations.tre.phylip") == 1);
+	fail_unless(file_exists("data/no_recombinations.tre.snp_sites.aln") == 1);
+
+	remove("data/no_recombinations.tre.tab");
+	remove("data/no_recombinations.tre.vcf");
+	remove("data/no_recombinations.tre.phylip");
+	remove("data/no_recombinations.tre.snp_sites.aln");
+}
+END_TEST
+
+START_TEST (check_gubbins_one_recombination)
+{
+	run_gubbins("data/one_recombination.aln.vcf", "data/one_recombination.aln.tre", "data/one_recombination.aln.tre","data/one_recombination.aln.snp_sites.aln");
+	fail_unless(file_exists("data/one_recombination.tre.tab") == 1);
+	fail_unless(file_exists("data/one_recombination.tre.vcf") == 1);
+	fail_unless(file_exists("data/one_recombination.tre.phylip") == 1);
+	fail_unless(file_exists("data/one_recombination.tre.snp_sites.aln") == 1);
+
+	remove("data/one_recombination.tre.tab");
+	remove("data/one_recombination.tre.vcf");
+	remove("data/one_recombination.tre.phylip");
+	remove("data/one_recombination.tre.snp_sites.aln");
+}
+END_TEST
+
+START_TEST (check_gubbins_multiple_recombinations)
+{
+	run_gubbins("data/multiple_recombinations.aln.vcf", "data/multiple_recombinations.aln.tre", "data/multiple_recombinations.aln.tre","data/multiple_recombinations.aln.snp_sites.aln");
+	fail_unless(file_exists("data/multiple_recombinations.tre.tab") == 1);
+	fail_unless(file_exists("data/multiple_recombinations.tre.vcf") == 1);
+	fail_unless(file_exists("data/multiple_recombinations.tre.phylip") == 1);
+	fail_unless(file_exists("data/multiple_recombinations.tre.snp_sites.aln") == 1);
+
+	remove("data/multiple_recombinations.tre.tab");
+	remove("data/multiple_recombinations.tre.vcf");
+	remove("data/multiple_recombinations.tre.phylip");
+	remove("data/multiple_recombinations.tre.snp_sites.aln");
+}
+END_TEST
+
 Suite * run_gubbins_suite(void)
 {
   Suite *s = suite_create ("Checking the gubbins functionality");
   TCase *tc_gubbins = tcase_create ("check_gubbins_produces_files");
   tcase_add_test (tc_gubbins, check_gubbins_produces_files);
+	tcase_add_test (tc_gubbins, check_gubbins_no_recombinations);
+	tcase_add_test (tc_gubbins, check_gubbins_one_recombination);
+	tcase_add_test (tc_gubbins, check_gubbins_multiple_recombinations);
   suite_add_tcase (s, tc_gubbins);
   return s;
 }

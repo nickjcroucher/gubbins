0f85c6dd241d9729b0fe958da0da129f182c790d
andrewjpage <andrewjpage@gmail.com>
2013-07-26 12:07:38 +0100

    test pairwise expected VCF and reconstructed fasta

--- python/gubbins/tests/test_external_dependancies.py
+++ python/gubbins/tests/test_external_dependancies.py
@@ -25,14 +25,34 @@ class TestExternalDependancies(unittest.TestCase):
   def test_pairwise_comparison(self):
     common.GubbinsCommon.pairwise_comparison('gubbins/tests/data/pairwise.aln','gubbins/tests/data/pairwise.aln','../src/gubbins','gubbins/tests/data/pairwise.aln','../external/fastml/programs/fastml/fastml  -mg -qf -b ')
     # Check the tree file is as expected
-    actual_file_content   = open('gubbins/tests/data/pairwise.tre',   'U').readlines()
+    actual_file_content   = open('gubbins/tests/data/pairwise.aln.tre',   'U').readlines()
     expected_file_content = open('gubbins/tests/data/pairwise_expected.tre', 'U').readlines()
     assert actual_file_content == expected_file_content
-    os.remove('gubbins/tests/data/pairwise.tre')
-
-
-    assert 1 == 0
-
+    
+    # Check the VCF file is as expected
+    actual_file_content   = open('gubbins/tests/data/pairwise.aln.tre.vcf',   'U').readlines()
+    expected_file_content = open('gubbins/tests/data/pairwise.aln.tre.vcf_expected', 'U').readlines()
+    assert actual_file_content == expected_file_content
+    
+    # Check the reconstruction of internal nodes
+    actual_file_content   = open('gubbins/tests/data/pairwise.aln.snp_sites.aln',   'U').readlines()
+    expected_file_content = open('gubbins/tests/data/pairwise.aln.snp_sites.aln_expected', 'U').readlines()
+    assert actual_file_content == expected_file_content
+    
+    os.remove('gubbins/tests/data/pairwise.aln.snp_sites.aln')
+    os.remove('gubbins/tests/data/pairwise.aln.tre')
+    os.remove('gubbins/tests/data/pairwise.aln.tre.ancestor.tre')
+    os.remove('gubbins/tests/data/pairwise.aln.tre.branch_snps.tab')
+    os.remove('gubbins/tests/data/pairwise.aln.tre.gff')
+    os.remove('gubbins/tests/data/pairwise.aln.tre.output_tree')
+    os.remove('gubbins/tests/data/pairwise.aln.tre.phylip')
+    os.remove('gubbins/tests/data/pairwise.aln.tre.prob.joint.txt')
+    os.remove('gubbins/tests/data/pairwise.aln.tre.seq.joint.txt')
+    os.remove('gubbins/tests/data/pairwise.aln.tre.snp_sites.aln')
+    os.remove('gubbins/tests/data/pairwise.aln.tre.stats')
+    os.remove('gubbins/tests/data/pairwise.aln.tre.tab')
+    os.remove('gubbins/tests/data/pairwise.aln.tre.vcf')
+    os.remove('log.txt')
 
   #
   #def test_delete_files_based_on_list_of_regexes(self):

2f65344c2da0506a6882723525b5ada143258502
Aidan Delaney <aidan@ontologyengineering.org>
2014-04-08 11:00:35 +0100

    All of our external dependencies are now provided as Ubuntu packages.   - the RaXML build uses the Makefile.generic which is the same as what is used by the Debian/Ubuntu packages.   - fastml is now packaged in PPA for Ubuntu saucy, we'll expand this coverage later.

--- release/manifests/saucyvm.pp
+++ release/manifests/saucyvm.pp
@@ -25,3 +25,21 @@ package {"python-setuptools":
 package {"cdbs":
     ensure => "installed"
     }
+
+package {"raxml":
+    ensure => "installed"
+    }
+
+package {"fasttree":
+    ensure => "installed"
+    }
+
+include apt
+
+# we need to pull in a packaged version of fastml for building.
+# Supplied by Aidan Delaney <aidan@ontologyengineering.org>, so blame him.
+apt::ppa { 'ppa:a-j-delaney/gubbins-ppa': }
+
+package {"fastml":
+        ensure => "installed"
+        }

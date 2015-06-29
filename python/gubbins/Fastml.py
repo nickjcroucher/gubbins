24d1d5a4e9f57ba4ba5398d20f36f20f2d883794
andrewjpage <andrewjpage@gmail.com>
2015-06-29 16:10:47 +0100

    detect version

--- python/gubbins/Fastml.py
+++ python/gubbins/Fastml.py
@@ -1,12 +1,43 @@
 import os
+import subprocess
 
 class Fastml(object):
   def __init__(self, fastml_exec = None):
       self.fastml_exec = fastml_exec
       self.fastml_version = None
       self.fastml_model = None
+      self.fastml_parameters = self.__calculate_parameters__()
+      
+  def __calculate_parameters__(self):
+      if(self.which(self.fastml_exec) == None)
+        return None
+      
+      if re.search('nucgtr', self.__run_without_options__()):
+          self.fastml_version = 3
+          self.fastml_model = 'g'
+      else
+          self.fastml_version = 2
+          
+      return self.fastml_exec + " -qf -b -a 0.00001 -m"+self.fastml_model+" "
+
+      
+  def __run_without_options__(self):
+      return subprocess.Popen(self.fastml_exec, stdout = subprocess.PIPE, shell=True).communicate()[0]
+      
+  def which(program):
+      executable = program.split(" ")
+      program = executable[0]
+      def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+      fpath, fname = os.path.split(program)
+      if fpath:
+        if is_exe(program):
+          return program
+      else:
+        for path in os.environ["PATH"].split(os.pathsep):
+          exe_file = os.path.join(path, program)
+          if is_exe(exe_file):
+            return exe_file
     
-  def fastml_parameters(self):
       return None
-      
-      
+        
--- python/gubbins/tests/test_fastml.py
+++ python/gubbins/tests/test_fastml.py
@@ -9,7 +9,7 @@ import unittest
 import re
 import os
 import subprocess
-from gubbins import Fastml
+from gubbins.Fastml import Fastml
 
 os.environ["PATH"] += os.pathsep + 'gubbins/tests/bin'
 
@@ -17,27 +17,27 @@ class TestFastml(unittest.TestCase):
     
   def test_no_fastml_installed(self):
       fastml_check = Fastml('exec_doesnt_exist')
-      self.assertEqual(fastml_check.fastml_version(), None)
+      self.assertEqual(fastml_check.fastml_version, None)
       self.assertEqual(fastml_check.fastml_model, None)
-      self.assertEqual(fastml_check.fastml_parameters(), None)
+      self.assertEqual(fastml_check.fastml_parameters, None)
       
   def test_fastml_3_installed(self):
       fastml_check = Fastml('dummy_fastml3')
-      self.assertEqual(fastml_check.fastml_version(), 3)
+      self.assertEqual(fastml_check.fastml_version, 3)
       self.assertEqual(fastml_check.fastml_model,'g')
-      self.assertEqual(fastml_check.fastml_parameters(), 'dummy_fastml3 -qf -b -a 0.00001 -mg')
+      self.assertEqual(fastml_check.fastml_parameters, 'dummy_fastml3 -qf -b -a 0.00001 -mg')
     
   def test_fastml_2_installed(self):
       fastml_check = Fastml('dummy_fastml2')
-      self.assertEqual(fastml_check.fastml_version(), 2)
+      self.assertEqual(fastml_check.fastml_version, 2)
       self.assertEqual(fastml_check.fastml_model, 'n')
-      self.assertEqual(fastml_check.fastml_parameters(), 'dummy_fastml2 -qf -b -a 0.00001 -mn')
+      self.assertEqual(fastml_check.fastml_parameters, 'dummy_fastml2 -qf -b -a 0.00001 -mn')
     
   def test_custom_fastml_2_installed(self):
       fastml_check = Fastml('dummy_custom_fastml2')
-      self.assertEqual(fastml_check.fastml_version(), 2)
+      self.assertEqual(fastml_check.fastml_version, 2)
       self.assertEqual(fastml_check.fastml_model,'g')
-      self.assertEqual(fastml_check.fastml_parameters(), 'dummy_custom_fastml2 -qf -b -a 0.00001 -mg')
+      self.assertEqual(fastml_check.fastml_parameters, 'dummy_custom_fastml2 -qf -b -a 0.00001 -mg')
 
 if __name__ == "__main__":
   unittest.main()

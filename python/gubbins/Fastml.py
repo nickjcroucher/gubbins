33ff6b5c12eafc017446518372c1314bbef8a226
andrewjpage <andrewjpage@gmail.com>
2015-06-29 15:49:55 +0100

    new Fastml file

---
+++ python/gubbins/Fastml.py
@@ -0,0 +1,12 @@
+import os
+
+class Fastml(object):
+  def __init__(self, fastml_exec = None):
+      self.fastml_exec = fastml_exec
+      self.fastml_version = None
+      self.fastml_model = None
+    
+  def fastml_parameters(self):
+      return None
+      
+      

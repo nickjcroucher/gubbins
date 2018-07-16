48b2203126d18de7e1cbdc1b2dbb480ed8458d96
puethe <cp15@sanger.ac.uk>
2018-07-16 18:20:13 +0100

    make consistent; multi-threaded RAxML

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -15,8 +15,11 @@ RAXML_ZIP_FILE="$RAXML_DIR.tar.gz"
 IQTREE_DIR="iqtree-$IQTREE_VERSION-Linux"
 IQTREE_ZIP_FILE="$IQTREE_DIR.tar.gz"
 
+FASTTREE_DIR="FastTree-$FASTTREE_VERSION"
+FASTTREE_SOURCE="$FASTTREE_DIR.c"
+
 RAXML_DOWNLOAD_URL="https://github.com/stamatak/standard-RAxML/archive/v$RAXML_VERSION.tar.gz"
-FASTTREE_DOWNLOAD_URL="http://www.microbesonline.org/fasttree/FastTree-$FASTTREE_VERSION.c"
+FASTTREE_DOWNLOAD_URL="http://www.microbesonline.org/fasttree/$FASTTREE_SOURCE"
 IQTREE_DOWNLOAD_URL="https://github.com/Cibiv/IQ-TREE/releases/download/v$IQTREE_VERSION/$IQTREE_ZIP_FILE"
 
 # Make an install location
@@ -40,7 +43,7 @@ download () {
 }
 
 download $RAXML_DOWNLOAD_URL $RAXML_ZIP_FILE
-download $FASTTREE_DOWNLOAD_URL "fasttree-${FASTTREE_VERSION}.c"
+download $FASTTREE_DOWNLOAD_URL $FASTTREE_SOURCE
 download $IQTREE_DOWNLOAD_URL $IQTREE_ZIP_FILE
 
 # Update dependencies
@@ -66,23 +69,26 @@ if [ ! -d $RAXML_DIR ]; then
 fi
 cd $RAXML_DIR
 if [ -e "raxmlHPC" ]; then
-  echo "Already built RAxML; skipping build"
+  echo "Already built single-processor RAxML; skipping build"
 else
   make -f Makefile.gcc
 fi
-
+if [ -e "raxmlHPC-PTHREADS" ]; then
+  echo "Already built multi-processor RAxML; skipping build"
+else
+  make -f Makefile.PTHREADS.gcc
+fi
 
 ## FastTree
 cd $build_dir
-fasttree_dir=${build_dir}/fasttree-${FASTTREE_VERSION}
-if [ ! -d $fasttree_dir ]; then
-  mkdir $fasttree_dir
+if [ ! -d $FASTTREE_DIR ]; then
+  mkdir $FASTTREE_DIR
 fi
-cd $fasttree_dir
-if [ -e "${fasttree_dir}/FastTree" ]; then
+cd $FASTTREE_DIR
+if [ -e "$FASTTREE_DIR/FastTree" ]; then
   echo "Skipping, FastTree already exists"
 else
-  gcc -O3 -finline-functions -funroll-loops -Wall -o FastTree ${build_dir}/fasttree-${FASTTREE_VERSION}.c  -lm
+  gcc -O3 -finline-functions -funroll-loops -Wall -o FastTree $build_dir/$FASTTREE_SOURCE  -lm
 fi
 
 ## IQTree
@@ -104,7 +110,7 @@ update_path () {
 }
 
 update_path $build_dir/$RAXML_DIR
-update_path ${fasttree_dir}
+update_path $build_dir/$FASTTREE_DIR
 update_path $build_dir/$IQTREE_DIR
 
 cd $start_dir

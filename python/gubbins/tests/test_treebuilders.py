910e463872405d7e31d174962d419af85ae1d36e
nickjcroucher <n.croucher@imperial.ac.uk>
2021-01-01 17:04:18 +0000

    Remove obsolete tests

--- python/gubbins/tests/test_treebuilders.py
+++ python/gubbins/tests/test_treebuilders.py
@@ -16,32 +16,8 @@ data_dir = os.path.join(modules_dir, 'tests', 'data')
 
 class TestStringConstruction(unittest.TestCase):
 
-    def test_fasttree_treebuilding_command(self):
-        fasttree = treebuilders.FastTree(8)
-        fasttree.executable = 'DDD'
-        fasttree.tree_building_parameters = ['EEE']
-        assert fasttree.tree_building_command('AAA', '', 'CCC') == 'DDD EEE -out CCC.tre AAA > /dev/null 2>&1'
-        fasttree.verbose = True
-        assert fasttree.tree_building_command('AAA', 'BBB', 'CCC') == 'DDD EEE -intree BBB -out CCC.tre AAA'
-
-    def test_iqtree_treebuilding_command(self):
-        iqtree = treebuilders.IQTree(8)
-        iqtree.executable = 'DDD'
-        iqtree.tree_building_parameters = ['EEE']
-        assert iqtree.tree_building_command('AAA', '', 'CCC') == 'DDD EEE -s AAA -pre CCC -nt 8 -quiet'
-        iqtree.verbose = True
-        iqtree.threads = 0
-        assert iqtree.tree_building_command('AAA', 'BBB', 'CCC') == 'DDD EEE -s AAA -pre CCC -nt AUTO -t BBB'
-
-    def test_iqtree_sequence_reconstruction_command(self):
-        iqtree = treebuilders.IQTree(8)
-        iqtree.executable = 'DDD'
-        iqtree.internal_sequence_reconstruction_parameters = ['EEE']
-        assert iqtree.internal_sequence_reconstruction_command('AAA', 'BBB', 'CCC') \
-               == 'DDD EEE -s AAA -pre CCC -nt 8 -te BBB -quiet'
-
     def test_iqtree_convert_raw_ancestral_states_to_fasta(self):
-        iqtree = treebuilders.IQTree(8)
+        iqtree = treebuilders.IQTree(1)
         iqtree.convert_raw_ancestral_states_to_fasta(os.path.join(data_dir, 'iqtree_ancestral.state'),
                                                      os.path.join(data_dir, 'iqtree_ancestral.fasta.actual'))
         assert filecmp.cmp(os.path.join(data_dir, 'iqtree_ancestral.fasta.actual'),
@@ -51,22 +27,6 @@ class TestStringConstruction(unittest.TestCase):
     def test_iqtree_replace_internal_node_label(self):
         iqtree = treebuilders.IQTree(8, 'AAA')
         assert iqtree.replace_internal_node_label('Node20') == 'AAA20'
-
-    def test_raxml_treebuilding_command(self):
-        raxml = treebuilders.RAxML(8)
-        raxml.executable = 'DDD'
-        raxml.tree_building_parameters = ['EEE']
-        assert raxml.tree_building_command('AAA', '', 'CCC') == 'DDD EEE -s AAA -n CCC -T 8 > /dev/null 2>&1'
-        raxml.verbose = True
-        raxml.threads = 1
-        assert raxml.tree_building_command('AAA', 'BBB', 'CCC') == 'DDD EEE -s AAA -n CCC -t BBB'
-
-    def test_raxml_sequence_reconstruction_command(self):
-        raxml = treebuilders.RAxML(8)
-        raxml.executable = 'DDD'
-        raxml.internal_sequence_reconstruction_parameters = ['EEE']
-        assert raxml.internal_sequence_reconstruction_command('AAA', 'BBB', 'CCC') \
-               == 'DDD EEE -s AAA -n CCC -T 8 -t BBB > /dev/null 2>&1'
 
     @unittest.mock.patch('gubbins.utils.choose_executable_based_on_processor')
     def test_raxml_select_executable_based_on_threads(self, mock_choice):
@@ -96,7 +56,7 @@ class TestStringConstruction(unittest.TestCase):
         os.remove(os.path.join(data_dir, 'raxml_ancestral.fasta.actual'))
 
     def test_raxml_replace_internal_node_label(self):
-        raxml = treebuilders.RAxML(8, internal_node_prefix='AAA')
+        raxml = treebuilders.RAxML(1, internal_node_prefix='AAA')
         assert raxml.replace_internal_node_label('Node20') == 'AAANode20'
 
 

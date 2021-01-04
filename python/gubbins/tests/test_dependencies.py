594cd56de237dc378f68797e9938ee9daeca54ef
nickjcroucher <n.croucher@imperial.ac.uk>
2021-01-04 22:22:26 +0000

    Add model fitting tests

--- python/gubbins/tests/test_dependencies.py
+++ python/gubbins/tests/test_dependencies.py
@@ -10,7 +10,7 @@ import os
 import glob
 import argparse
 import pkg_resources
-from gubbins import common
+from gubbins import common, run_gubbins
 
 modules_dir = os.path.dirname(os.path.abspath(common.__file__))
 data_dir = os.path.join(modules_dir, 'tests', 'data')
@@ -18,36 +18,157 @@ data_dir = os.path.join(modules_dir, 'tests', 'data')
 
 class TestExternalDependencies(unittest.TestCase):
 
+    # Test individual tree builders
     def test_fasttree(self):
-        parser = self.default_arg_parse()
+        parser = run_gubbins.parse_input_args()
         common.parse_and_run(parser.parse_args(["--tree-builder", "fasttree",
                                                 os.path.join(data_dir, 'multiple_recombinations.aln')]))
         self.check_for_output_files('multiple_recombinations')
         self.cleanup('multiple_recombinations')
 
     def test_iqtree(self):
-        parser = self.default_arg_parse()
+        parser = run_gubbins.parse_input_args()
         common.parse_and_run(parser.parse_args(["--tree-builder", "iqtree",
                                                 os.path.join(data_dir, 'multiple_recombinations.aln')]))
         self.check_for_output_files('multiple_recombinations')
         self.cleanup('multiple_recombinations')
 
     def test_raxml(self):
-        parser = self.default_arg_parse()
+        parser = run_gubbins.parse_input_args()
         common.parse_and_run(parser.parse_args(["--tree-builder", "raxml",
                                                 os.path.join(data_dir, 'multiple_recombinations.aln')]))
         self.check_for_output_files('multiple_recombinations')
         self.cleanup('multiple_recombinations')
 
+    def test_raxmlng(self):
+        parser = run_gubbins.parse_input_args()
+        common.parse_and_run(parser.parse_args(["--tree-builder", "raxmlng",
+                                                os.path.join(data_dir, 'multiple_recombinations.aln')]))
+        self.check_for_output_files('multiple_recombinations')
+        self.cleanup('multiple_recombinations')
+
+    def test_rapidnj(self):
+        parser = run_gubbins.parse_input_args()
+        common.parse_and_run(parser.parse_args(["--tree-builder", "rapidnj",
+                                                "--model","JC",
+                                                os.path.join(data_dir, 'multiple_recombinations.aln')]))
+        self.check_for_output_files('multiple_recombinations')
+        self.cleanup('multiple_recombinations')
+
+    # Test initial star tree
+    def test_starting_star_fasttree(self):
+        parser = run_gubbins.parse_input_args()
+        common.parse_and_run(parser.parse_args(["--first-tree-builder","star",
+                                                "--tree-builder", "fasttree",
+                                                os.path.join(data_dir, 'multiple_recombinations.aln')]))
+        self.check_for_output_files('multiple_recombinations')
+        self.cleanup('multiple_recombinations')
+
+    def test_starting_star_iqtree(self):
+        parser = run_gubbins.parse_input_args()
+        common.parse_and_run(parser.parse_args(["--first-tree-builder","star",
+                                                "--tree-builder", "iqtree",
+                                                os.path.join(data_dir, 'multiple_recombinations.aln')]))
+        self.check_for_output_files('multiple_recombinations')
+        self.cleanup('multiple_recombinations')
+
+    def test_starting_star_raxml(self):
+        parser = run_gubbins.parse_input_args()
+        common.parse_and_run(parser.parse_args(["--first-tree-builder","star",
+                                                "--tree-builder", "raxml",
+                                                os.path.join(data_dir, 'multiple_recombinations.aln')]))
+        self.check_for_output_files('multiple_recombinations')
+        self.cleanup('multiple_recombinations')
+
+    def test_starting_star_raxmlng(self):
+        parser = run_gubbins.parse_input_args()
+        common.parse_and_run(parser.parse_args(["--first-tree-builder","star",
+                                                "--tree-builder", "raxmlng",
+                                                "--verbose",
+                                                os.path.join(data_dir, 'multiple_recombinations.aln')]))
+        self.check_for_output_files('multiple_recombinations')
+        self.cleanup('multiple_recombinations')
+
+    def test_starting_star_rapidnj(self):
+        parser = run_gubbins.parse_input_args()
+        common.parse_and_run(parser.parse_args(["--first-tree-builder","star",
+                                                "--tree-builder", "rapidnj",
+                                                "--model","JC",
+                                                os.path.join(data_dir, 'multiple_recombinations.aln')]))
+        self.check_for_output_files('multiple_recombinations')
+        self.cleanup('multiple_recombinations')
+        
+    # Test sequence reconstruction
+    def test_raxml_seq_recon(self):
+        parser = run_gubbins.parse_input_args()
+        common.parse_and_run(parser.parse_args(["--tree-builder", "raxml",
+                                                "--seq-recon", "raxml",
+                                                "--mar",
+                                                os.path.join(data_dir, 'multiple_recombinations.aln')]))
+        self.check_for_output_files('multiple_recombinations')
+        self.cleanup('multiple_recombinations')
+
+    def test_iqtree_seq_recon(self):
+        parser = run_gubbins.parse_input_args()
+        common.parse_and_run(parser.parse_args(["--tree-builder", "raxml",
+                                                "--seq-recon", "iqtree",
+                                                "--mar",
+                                                os.path.join(data_dir, 'multiple_recombinations.aln')]))
+        self.check_for_output_files('multiple_recombinations')
+        self.cleanup('multiple_recombinations')
+
+    def test_raxmlng_seq_recon(self):
+        parser = run_gubbins.parse_input_args()
+        common.parse_and_run(parser.parse_args(["--tree-builder", "raxml",
+                                                "--seq-recon", "raxmlng",
+                                                "--mar",
+                                                os.path.join(data_dir, 'multiple_recombinations.aln')]))
+        self.check_for_output_files('multiple_recombinations')
+        self.cleanup('multiple_recombinations')
+
+    # Test model fitting
+    def test_fasttree_model_fit(self):
+        parser = run_gubbins.parse_input_args()
+        common.parse_and_run(parser.parse_args(["--tree-builder", "raxml",
+                                                "--model-fitter", "fasttree",
+                                                os.path.join(data_dir, 'multiple_recombinations.aln')]))
+        self.check_for_output_files('multiple_recombinations')
+        self.cleanup('multiple_recombinations')
+
+    def test_iqtree_model_fit(self):
+        parser = run_gubbins.parse_input_args()
+        common.parse_and_run(parser.parse_args(["--tree-builder", "raxml",
+                                                "--model-fitter", "iqtree",
+                                                os.path.join(data_dir, 'multiple_recombinations.aln')]))
+        self.check_for_output_files('multiple_recombinations')
+        self.cleanup('multiple_recombinations')
+
+    def test_raxml_model_fit(self):
+        parser = run_gubbins.parse_input_args()
+        common.parse_and_run(parser.parse_args(["--tree-builder", "raxml",
+                                                "--model-fitter", "raxml",
+                                                os.path.join(data_dir, 'multiple_recombinations.aln')]))
+        self.check_for_output_files('multiple_recombinations')
+        self.cleanup('multiple_recombinations')
+
+    def test_raxmlng_model_fit(self):
+        parser = run_gubbins.parse_input_args()
+        common.parse_and_run(parser.parse_args(["--tree-builder", "raxml",
+                                                "--model-fitter", "raxmlng",
+                                                os.path.join(data_dir, 'multiple_recombinations.aln')]))
+        self.check_for_output_files('multiple_recombinations')
+        self.cleanup('multiple_recombinations')
+
+    # Test renaming of final output
     def test_rename_final_output(self):
-        parser = self.default_arg_parse()
+        parser = run_gubbins.parse_input_args()
         common.parse_and_run(parser.parse_args(["--prefix", "different_prefix",
                                                 os.path.join(data_dir, 'multiple_recombinations.aln')]))
         self.check_for_output_files('different_prefix')
         self.cleanup('different_prefix')
 
     def test_cleanup(self):
-        parser = self.default_arg_parse()
+        parser = run_gubbins.parse_input_args()
         common.parse_and_run(parser.parse_args([os.path.join(data_dir, 'multiple_recombinations.aln')]))
         self.cleanup('multiple_recombinations')
         assert not glob.glob('multiple_recombinations.aln.*')
@@ -70,110 +191,6 @@ class TestExternalDependencies(unittest.TestCase):
         regex_to_remove = prefix + ".*"
         for file in glob.glob(regex_to_remove):
             os.remove(file)
-
-    @staticmethod
-    def default_arg_parse():
-        # Keep this up to date with run_gubbins.py!
-        parser = argparse.ArgumentParser(
-            description='Croucher N. J., Page A. J., Connor T. R., Delaney A. J., Keane J. A., Bentley S. D., Parkhill J., '
-                        'Harris S.R. "Rapid phylogenetic analysis of large samples of recombinant bacterial whole genome '
-                        'sequences using Gubbins". Nucleic Acids Res. 2015 Feb 18;43(3):e15. doi: 10.1093/nar/gku1196.',
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-        ioGroup = parser.add_argument_group('Input and output options')
-        ioGroup.add_argument('alignment_filename',        help='Multifasta alignment file')
-        ioGroup.add_argument('--prefix',            '-p', help='Add a prefix to the final output filenames')
-        ioGroup.add_argument('--starting-tree',     '-s', help='Starting tree')
-        ioGroup.add_argument('--use-time-stamp',    '-u', help='Use a time stamp in file names', action='store_true')
-                            
-        dataGroup = parser.add_argument_group('Data processing options')
-        dataGroup.add_argument('--pairwise',              help='Compare two sequences (without using a tree)',
-                                                          default = False, action = 'store_true') # fasttree model fit, star phylogeny, one iteration
-        dataGroup.add_argument('--filter-percentage','-f',
-                                                          help='Filter out taxa with more than this percentage of gaps',
-                                                          type=float, default=25.0)
-        dataGroup.add_argument('--remove-identical-sequences',
-                                                    '-d', help='Remove identical sequences', action='store_true')
-        dataGroup.add_argument('--threads',         '-c', help='Number of threads to run with RAXML, but only if a PTHREADS '
-                                                          'version is available', type=int,  default=1)
-        dataGroup.add_argument('--verbose',         '-v', help='Turn on debugging', action='store_true')
-        dataGroup.add_argument('--no-cleanup',      '-n', help='Do not cleanup intermediate files', action='store_true')
-
-        treeGroup = parser.add_argument_group('Tree building options')
-        treeGroup.add_argument('--tree-builder',    '-t', help='Application to use for tree building',
-                                                          default='raxml',
-                                                          choices=['raxml', 'raxmlng', 'iqtree', 'fasttree', 'hybrid', 'rapidnj'])
-        treeGroup.add_argument('--tree-args',             help='Quoted string of further arguments passed to tree building algorithm'
-                                                          ' (start string with a space if there is a risk of being interpreted as a flag)',
-                                                          default = None)
-        treeGroup.add_argument('--first-tree-builder',    help='Application to use for building the first tree',
-                                                          default=None,
-                                                          choices=['raxml', 'raxmlng', 'iqtree', 'fasttree', 'rapidnj', 'star'])
-        treeGroup.add_argument('--first-tree-args',       help='Further arguments passed to first tree building algorithm',
-                                                          default = None)
-        treeGroup.add_argument('--outgroup',        '-o', help='Outgroup name for rerooting. A list of comma separated '
-                                                          'names can be used if they form a clade')
-        treeGroup.add_argument('--bootstrap',       '-#', help='Number of bootstrap replicates to perform with final alignment '
-                                                          '[default = 0]', type = int, default = 0)
-        treeGroup.add_argument('--transfer-bootstrap',    help='Calculate bootstrap supporting transfer bootstrap expectation '
-                                                          '[default = False]', default = False, action = 'store_true')
-        treeGroup.add_argument('--sh-test',               help='Perform an SH test of node likelihoods', default = False,
-                                                          action = 'store_true')
-                                                              
-        modelGroup = parser.add_argument_group('Nucleotide substitution model options')
-        modelGroup.add_argument('--model-fitter',   '-F', help='Application to use for model fitting [default = same as'
-                                                          ' tree builder if possible, else raxml]',
-                                                          default = None,
-                                                          choices=['raxml', 'raxmlng', 'iqtree', 'fasttree', None])
-        modelGroup.add_argument('--model',          '-M', help='Nucleotide substitution model (not all available for all'
-                                                          'tree building algorithms',
-                                                          default='GTRGAMMA',
-                                                          choices=['JC','K2P','HKY','GTR','GTRGAMMA','GTRCAT'])
-        modelGroup.add_argument('--model-args',           help='Quoted string of further arguments passed to model fitting algorithm'
-                                                          ' (start string with a space if there is a risk of being interpreted as a flag)'
-                                                          '(default = same as --tree-builder-args)',
-                                                          default=None)
-        modelGroup.add_argument('--custom-model',         help='String corresponding to a substitution model for the selected tree'
-                                                          ' building algorithm [default = None]', default = None)
-        modelGroup.add_argument('--first-model-fitter',   help='Application to use for model fitting in first iteration'
-                                                          ' [default = same as tree builder if possible, else raxml]',
-                                                          default = None,
-                                                          choices=['raxml', 'raxmlng', 'iqtree', 'fasttree', None])
-        modelGroup.add_argument('--first-model',          help='Nucleotide substitution model used for first tree',
-                                                          default=None,
-                                                          choices=['JC','K2P','HKY','GTR','GTRGAMMA','GTRCAT'])
-        modelGroup.add_argument('--first-model-args',     help='Further arguments passed to model fitting algorithm used in first'
-                                                          'iteration (default = same as --first-tree-builder-args)',
-                                                          default=None)
-        modelGroup.add_argument('--custom-first-model',   help='String corresponding to a substitution model for the selected tree'
-                                                          ' building algorithm for the first iteration [default = None]',
-                                                          default = None)
-
-        reconGroup = parser.add_argument_group('Ancestral sequence reconstruction options')
-        reconGroup.add_argument('--mar',                  help='Use marginal, rather than joint, ancestral reconstruction',
-                                                          action='store_true')
-        reconGroup.add_argument('--seq-recon',            help='Algorithm to use for marginal reconstruction [default = '
-                                                          'same as tree builder if possible, else raxml]',
-                                                          default=None,
-                                                          choices=['raxml', 'raxmlng', 'iqtree', None])
-        reconGroup.add_argument('--seq-recon-args',       help='Further arguments passed to sequence reconstruction algorithm'
-                                                          ' (start string with a space if there is a risk of being interpreted as a flag)',
-                                                          default=None)
-                                                                
-        gubbinsGroup = parser.add_argument_group('Recombination detection options')
-        gubbinsGroup.add_argument('--min-snps',     '-m', help='Min SNPs to identify a recombination block',
-                                                          type=int,
-                                                          default = 3)
-        gubbinsGroup.add_argument('--min-window-size','-a',
-                                                          help='Minimum window size', type=int, default=100)
-        gubbinsGroup.add_argument('--max-window-size','-b',
-                                                          help='Maximum window size', type=int, default=10000)
-
-        stopGroup = parser.add_argument_group('Algorithm stop options')
-        stopGroup.add_argument('--iterations',      '-i', help='Maximum No. of iterations', type=int, default=5)
-        stopGroup.add_argument('--converge-method', '-z', help='Criteria to use to know when to halt iterations',
-                            default='weighted_robinson_foulds', choices=['weighted_robinson_foulds', 'robinson_foulds',
-                                                                         'recombination'])
-        return parser
 
 if __name__ == "__main__":
     unittest.main(buffer=True)

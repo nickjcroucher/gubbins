874799ce5aa70a81ed87ff796ba6309d0a1767fc
Nick Croucher <n.croucher@imperial.ac.uk>
2021-10-28 10:42:37 +0100

    Switch input files

--- python/scripts/extract_gubbins_clade_statistics.py
+++ python/scripts/extract_gubbins_clade_statistics.py
@@ -42,9 +42,15 @@ def get_options():
     parser.add_argument('--clades',
                         help = 'Two column file assigning isolates (first column) to clades (second column)',
                         required = True)
-    parser.add_argument('--csv',
-                        help = 'per_branch_statistics.csv file output by Gubbins',
+    parser.add_argument('--gff',
+                        help = 'recombination prediction GFF file output by Gubbins',
                         required = True)
+    parser.add_argument('--snps',
+                        help = 'branch base reconstruction EMBL file output by Gubbins',
+                        required = True)
+    parser.add_argument('--exclude-regions',
+                        help = 'Two column file specifying start and end of regions to be excluded',
+                        required = False)
     parser.add_argument('--tree',
                         help = 'Labelled tree output by Gubbins',
                         required = True)
@@ -72,18 +78,58 @@ if __name__ == "__main__":
             else:
                 sys.stderr.write('Line needs two columns: ' + line + '\n')
     
-    # Store information on branches
-    node_info = {}
-    info_labels = ['total_snps','rec_snps','mutation_snps','recombinations']
-    with open(args.csv,'r') as info_csv:
-        for line in info_csv.readlines():
+    # Store SNP information
+    node_snps = {}
+    snp_total = 0
+    with open(args.snps,'r') as snp_file:
+        pos = 0
+        for line in snp_file.readlines():
             info = line.strip().split()
-            if info[0] != 'Node':
-                node_info[info[0]] = {}
-                for index,label in enumerate(info_labels):
-                    node_info[info[0]][label] = int(info[index+1])
+            if info[1] == 'variation':
+                pos = int(info[2])
+                snp_total += 1
+            if info[1].startswith('/node='):
+                node = info[1].replace('"','').split('->')
+                if node[1] in node_snps:
+                    node_snps[node[1]].append(pos)
+                else:
+                    node_snps[node[1]] = [pos]
+
+    # Store recombination information
+    node_rec_starts = {}
+    node_rec_ends = {}
+    with open(args.gff,'r') as gff_file:
+        for line in gff_file.readlines():
+            if not line.startswith('##'):
+                info = line.rstrip().split('\t')
+                start = int(info[3])
+                end = int(info[4])
+                node = info[8].split(';')[0].replace('"','').split('->')[1]
+                if node not in node_rec_starts:
+                    node_rec_starts[node] = [start]
+                    node_rec_ends[node] = [end]
+                else:
+                    node_rec_starts[node].append(start)
+                    node_rec_ends[node].append(end)
+    
+    # Divide SNPs into recombinant and non-recombinant
+    rec_snps = {node:0 for node in node_snps}
+    pm_snps = {node:0 for node in node_snps}
+    for node in node_snps:
+        for p in node_snps[node]:
+            rec_snp = False
+            if node in node_rec_starts:
+                for s,e in zip(node_rec_starts[node],node_rec_ends[node]):
+                    if p >= s and p <= e:
+                        rec_snp = True
+                        break
+            if rec_snp:
+                rec_snps[node] += 1
+            else:
+                pm_snps[node] += 1
     
     # Parse tree
+    info_labels = ['total_snps','rec_snps','mutation_snps','recombinations']
     tree_info_labels = ['n_taxa','n_branches','branch_length']
     tree = dendropy.Tree.get(path = args.tree,
                              schema = 'newick',
@@ -107,10 +153,15 @@ if __name__ == "__main__":
                     clade_info['branch_length'] += node.edge_length
                     if node.is_leaf():
                         clade_info['n_taxa'] += 1
-                        for datum in info_labels:
-                            clade_info[datum] += node_info[node.taxon.label][datum]
+                        node_label_string = node.taxon.label
                     else:
-                        for datum in info_labels:
-                            clade_info[datum] += node_info[node.label][datum]
+                        node_label_string = node.label
+                    if node_label_string in node_snps:
+                        clade_info['total_snps'] += len(node_snps[node_label_string])
+                        clade_info['rec_snps'] += rec_snps[node_label_string]
+                        clade_info['mutation_snps'] += pm_snps[node_label_string]
+                    if node_label_string in node_rec_starts:
+                        clade_info['recombinations'] += len(node_rec_starts[node_label_string])
+                    
             out_file.write(','.join([str(clade_info[label]) for label in info_labels + tree_info_labels]))
             out_file.write('\n')

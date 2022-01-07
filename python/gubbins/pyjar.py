52fefc27925d225d194f71657a07e96463985fe7
jdaeth274 <joshdaeth@yahoo.co.uk>
2022-01-07 11:59:47 +0000

    allowing for different alignment type input

--- python/gubbins/pyjar.py
+++ python/gubbins/pyjar.py
@@ -949,6 +949,7 @@ def jar(alignment = None,
     print_file.write("End Alignment array creation " + str(datetime.datetime.now()) + "\n")
     print_file.write("End mem usage (GB): " + str(psutil.Process(os.getpid()).memory_info().rss / 1024 ** 3) + "\n")
     print_file.write("Size of new_aln_array " + str(new_aln_array.shape) + "\n")
+    print_file.write("Memory size of new aln_array: " + str(new_aln_array.nbytes) + " (bytes)" + "\n")
     print_file.write("|/-\|/-\||/-\|/-\||/-\|/-\||/-\|/-\|" + "\n")
     print_file.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + "\n")
     print_file.close()

dfceba9feca08e7b04ca8b6ee4e529e7350db84b
Nick Croucher <n.croucher@imperial.ac.uk>
2022-01-18 11:55:10 +0000

    Implement inefficient qsort

--- src/csv_of_snp_sites.c
+++ src/csv_of_snp_sites.c
@@ -25,6 +25,46 @@
 #include "parse_phylip.h"
 #include "string_cat.h"
 
+struct cmpargs {
+    int *arr;
+    char **words;
+} args;
+
+// String comparison code
+int qcmp(const void *x, const void *y) {
+    
+    const char *pp1 = *(const char**)x;
+    const char *pp2 = *(const char**)y;
+    printf("PP1 is %s\n",pp1);
+    int compval = strcmp(pp1,pp2);
+//    return strncmp(pp1, pp2, 14);
+    return compval;
+    
+//    const char *(x_str) = *(const char **)x;
+//    const char *(y_str) = *(const char **)y;
+//    return strcmp(x_str, y_str);
+    
+//    const int ix = *(const int *)x;
+//    const int iy = *(const int *)y;
+//    return ix - iy;
+//    const char *(x_str) = *(const char **)bases[*ix];
+//    const char *(y_str) = *(const char **)bases[*iy];
+//    return strcmp(x_str, y_str);
+//    const char *(x_str) = *(const char **)bases[*(const int*)x];
+//    const char *(y_str) = *(const char **)bases[*(const int*)y];
+//    return strcmp(x_str, y_str);
+}
+
+//int cmpfunc(const void * a, const void * b, void *_args) {
+//    struct cmpargs args = _args;
+//
+//    int *a1 = a;
+//    int *a2 = b;
+//
+//    int idx1 = a - args->arr;
+//    int idx2 = b - args->arr;
+//}
+
 void create_csv_of_snp_sites(char filename[], int number_of_snps, char ** bases_for_snps, int* snp_location, char ** sequence_names, int number_of_samples,int internal_nodes[]) {
     
     // Patterns CSV file
@@ -45,44 +85,51 @@ void create_csv_of_snp_sites(char filename[], int number_of_snps, char ** bases_
     concat_strings_created_with_malloc(positions_base_filename,positions_extension);
     positions_file_pointer = fopen(positions_base_filename, "w");
 
-    // Avoid any large data structures to reduce peak memory
+    // Copy pointers for sorting
+    char** sorted_base_patterns = malloc(number_of_snps * sizeof(char*));
     int i = 0;
     for (i = 0; i < number_of_snps; i++)
     {
-        int j = 0;
-        for (j = 0; j < number_of_snps; j++)
+        sorted_base_patterns[i] = bases_for_snps[i];
+    }
+    
+    // Create structure for sorting
+    args.arr = sorted_base_patterns;
+    args.words = bases_for_snps;
+
+    // Sort the pointers to strings
+    int stringLen = sizeof(bases_for_snps) / sizeof(char*);
+    
+    qsort(sorted_base_patterns, number_of_snps, sizeof(char*), qcmp);
+//    qsort_r(sorted_base_patterns, number_of_snps, sizeof(int*), bases_for_snps, qcmp);
+//    qsort_r(sorted_base_patterns, number_of_snps, sizeof(int), &args, qcmp);
+    
+    // Avoid any large data structures to reduce peak memory
+    int j = 0;
+    for (j = 0; j < number_of_snps; j++)
+    {
+        // Identify if value is unique using sorted array
+        if (j == 0 || strcmp(sorted_base_patterns[j-1],sorted_base_patterns[j]) != 0)
         {
-            if (strcmp(bases_for_snps[i],bases_for_snps[j]) == 0) {
-                // If the pattern has not been observed earlier in the list,
-                // print to file
-                if (j == i)
+            // Print base pattern to file
+            fprintf(patterns_file_pointer, "%s\n", sorted_base_patterns[j]);
+            // Print each position at which the pattern is observed
+            int k = 0;
+            for (k = 0; k < number_of_snps; k++)
+            {
+                if (strcmp(bases_for_snps[j],bases_for_snps[k]) == 0)
                 {
-                    fprintf(patterns_file_pointer, "%s\n", bases_for_snps[i]);
-                    fprintf(positions_file_pointer, "%i", snp_location[i]);
-                }
-                // Stop iterating if base pattern has already been seen
-                // earlier in the list
-                if (j < i)
-                {
-                    break;
-                }
-                // Print out other locations at which the same string
-                // is observed on the first observation of the string
-                else if (j > i)
-                {
-                    fprintf(positions_file_pointer, ",%i", snp_location[j]);
+                    fprintf(positions_file_pointer, "%i\t", snp_location[k]);
                 }
             }
-        }
-        if (j >= i)
-        {
             fprintf(positions_file_pointer, "\n");
         }
     }
-    
+
     // Tidy up memory
     fclose(patterns_file_pointer);
     fclose(positions_file_pointer);
+    free(sorted_base_patterns);
     free(patterns_base_filename);
     free(positions_base_filename);
     
--- src/csv_of_snp_sites.h
+++ src/csv_of_snp_sites.h
@@ -21,6 +21,9 @@
 #ifndef _CSV_OF_SNP_SITES_
 #define _CSV_OF_SNP_SITES_
 
+//int qcmp(void* bases, const void *x, const void *y);
+int qcmp(const void *x, const void *y);
+
 void create_csv_of_snp_sites(char filename[], int number_of_snps, char ** bases_for_snps, int * snp_location, char ** sequence_names, int number_of_samples,int internal_nodes[]);
 
 #endif

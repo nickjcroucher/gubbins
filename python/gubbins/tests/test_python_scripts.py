7c94c9c3c0a65c54dfaaa7bfdbbb669e070b96a7
jdaeth274 <joshdaeth@yahoo.co.uk>
2022-02-16 15:23:38 +0000

    tweaking alignment_checker test

--- python/gubbins/tests/test_python_scripts.py
+++ python/gubbins/tests/test_python_scripts.py
@@ -21,11 +21,12 @@ class TestPythonScripts(unittest.TestCase):
 
     def test_alignment_checker(self):
         small_aln = os.path.join(data_dir, "valid_alignment.aln")
-        output_file = os.path.join(working_dir, "valid_alignment_test.csv")
-        aln_cmd = "alignment_checker.py --aln " + small_aln + " --out " + output_file
+        output_file = os.path.join(working_dir, "valid_alignment_test")
+        output_csv = os.path.join(working_dir, "valid_alignment_test.csv")
+        aln_cmd = "alignment_checker.py --aln " + small_aln + " --out " + output_file 
         subprocess.check_call(aln_cmd, shell=True)
-        assert self.md5_check(output_file, "b409830c1ee7772d551c13abe5cde7a7")
-        os.remove(output_file)
+        assert self.md5_check(output_csv, "b409830c1ee7772d551c13abe5cde7a7")
+        os.remove(output_csv)
 
     @staticmethod
     def md5_check(file_path, correct_md5sum):

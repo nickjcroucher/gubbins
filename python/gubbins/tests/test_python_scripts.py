8cfb4d94b3e872c19bb199f84835cbc4bfb6802b
Nick Croucher <n.croucher@imperial.ac.uk>
2022-02-23 06:28:03 +0000

    Change alignment checking script name

--- python/gubbins/tests/test_python_scripts.py
+++ python/gubbins/tests/test_python_scripts.py
@@ -26,7 +26,7 @@ class TestPythonScripts(unittest.TestCase):
         output_file = os.path.join(working_dir, "valid_alignment_test")
         output_csv = os.path.join(working_dir, "valid_alignment_test.csv")
         test_csv = os.path.join(data_dir, "test_valid_output.csv")
-        aln_cmd = "alignment_checker.py --aln " + small_aln + " --out " + output_file 
+        aln_cmd = "gubbins_alignment_checker.py --aln " + small_aln + " --out " + output_file
         subprocess.check_call(aln_cmd, shell=True)
         assert self.md5_check(output_csv, test_csv)
         os.remove(output_csv)
@@ -46,6 +46,7 @@ class TestPythonScripts(unittest.TestCase):
         test_aln = os.path.join(data_dir, "multiple_recombinations_clade_extract.aln")
         test_gff = os.path.join(data_dir, "multiple_recombinations_clade_extract.gff")
         test_tree = os.path.join(data_dir, "multiple_recombinations_clade_extract.tree")
+        # Script name
         extract_clade_cmd = "extract_gubbins_clade.py --list " + clade_list + " --aln " + multiple_aln +\
             " --gff " + multiple_gff + " --tree " + multiple_tree + " --out " + base_path +  " --out-fmt fasta"
         subprocess.check_call(extract_clade_cmd, shell=True)
@@ -63,8 +64,8 @@ class TestPythonScripts(unittest.TestCase):
         out_aln = os.path.join(data_dir, "multiple_recombinations_mask.aln")
         ## Get the test file 
         test_aln = os.path.join(data_dir, "masking_multiple.aln")
-        
-        extract_clade_cmd = "mask_gubbins_aln.py --aln " + multiple_aln +\
+        # Script name
+        extract_clade_cmd = "extract_gubbins_clade.py --aln " + multiple_aln +\
             " --gff " + multiple_gff + " --out " + out_aln +  " --out-fmt fasta"
         subprocess.check_call(extract_clade_cmd, shell=True)
         assert self.md5_check(out_aln, test_aln)
@@ -79,6 +80,7 @@ class TestPythonScripts(unittest.TestCase):
         fasta_loc = './ska_fasta_list.txt'
         ref_seq = os.path.join(preprocess_dir, 'sequence_t1.fasta')
         aln_out = os.path.join(preprocess_dir, 'ska_test_aln.aln')
+        # Script name
         ska_cmd = "generate_ska_alignment.py --fasta " + fasta_loc +\
             " --reference " + ref_seq + " --out " + aln_out +\
                 " --k 6"
@@ -92,8 +94,6 @@ class TestPythonScripts(unittest.TestCase):
         self.cleanup('ska_test')
         os.remove(aln_out)
         assert exit_code == 0
-
-
 
     @staticmethod
     def check_for_output_files(prefix):
--- python/setup.py
+++ python/setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
         'scripts/generate_ska_alignment.py',
         'scripts/extract_gubbins_clade.py',
         'scripts/mask_gubbins_aln.py',
-        'scripts/alignment_checker.py'
+        'scripts/gubbins_alignment_checker.py'
     ],
     tests_require=[
         "pytest >= 4.6",

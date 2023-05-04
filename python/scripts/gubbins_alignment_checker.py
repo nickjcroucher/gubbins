52ad7dd7a339770628b4201112d8bb1db87ee867
Nick Croucher <n.croucher@imperial.ac.uk>
2023-05-04 17:47:03 +0100

    Enable reformatting of alignments

--- python/scripts/gubbins_alignment_checker.py
+++ python/scripts/gubbins_alignment_checker.py
@@ -37,6 +37,7 @@ def parse_input_args():
     parser.add_argument('--out-aln',
                         dest="out_aln",
                         help='Reformatted alignment filename',
+                        default = None,
                         required=False)
     parser.add_argument('--out',
                         '-o',
@@ -48,7 +49,7 @@ def parse_input_args():
 
 def main(input_args):
 
-    ## Lets set up the csv
+    # Read the number of rows in the alignment
     row_num = 0
     tot_lines = 0
     with open(input_args.aln, "r") as aln_file:
@@ -57,6 +58,21 @@ def main(input_args):
                 row_num += 1
             tot_lines += 1
 
+    # Filter alignment if requested
+    aln_filename = input_args.aln
+    if input_args.out_aln:
+        with open(input_args.aln, "r") as aln_file, \
+                open(input_args.out_aln, "w") as out_aln_file:
+            for line in aln_file:
+                if re.search("^>", line):
+                    name = line.replace("#","_").replace(":","_").replace(">","").rstrip().split()
+                    out_aln_file.write('>' + name[0] + '\n')
+                else:
+                    sequence = line.upper().rstrip()
+                    sequence = re.sub('[^ACGTN-]','N',sequence)
+                    out_aln_file.write(sequence + '\n')
+        aln_filename = input_args.out_aln
+
     # Going to use counter in a first pass to store sequence counts and the range of sequences
     total_base_counts = []
     iso_data = []
@@ -64,7 +80,7 @@ def main(input_args):
     tot_length_str = str(tot_lines)
     print("Running through alignment file: %s" % input_args.aln)
     print()
-    with open(input_args.aln, "r") as aln_file:
+    with open(aln_filename, "r") as aln_file:
         for index,line in enumerate(aln_file):
             num_zeros = len(tot_length_str) - len(str(index + 1))
             fmt_index = (("0" * num_zeros) + str(index + 1))

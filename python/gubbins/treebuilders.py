6c9c69399d305c28cf04ee143cc696f07c0945b6
Nick Croucher <n.croucher@imperial.ac.uk>
2025-03-27 11:07:42 +0000

    Fix quote marks

--- python/gubbins/treebuilders.py
+++ python/gubbins/treebuilders.py
@@ -477,13 +477,13 @@ class RAxML:
         if self.model == 'JC':
             command.extend(["-m", "GTRCAT+ASC_FELS{" + str(invariant_sites) + "}","--JC69"])
         elif self.model == 'K2P':
-            command.extend(["-m", "GTRCAT+ASC_FELS{" + str(invariant_sites) + "}"","--K80"])
+            command.extend(["-m", "GTRCAT+ASC_FELS{" + str(invariant_sites) + "}","--K80"])
         elif self.model == 'HKY':
-            command.extend(["-m", "GTRCAT+ASC_FELS{" + str(invariant_sites) + "}"","--HKY85"])
+            command.extend(["-m", "GTRCAT+ASC_FELS{" + str(invariant_sites) + "}","--HKY85"])
         elif self.model == 'GTRCAT':
-            command.extend(["-m","GTRCAT+ASC_FELS{" + str(invariant_sites) + "}"", "-V"])
+            command.extend(["-m","GTRCAT+ASC_FELS{" + str(invariant_sites) + "}", "-V"])
         elif self.model == 'GTRGAMMA':
-            command.extend(["-m","GTRGAMMA+ASC_FELS{" + str(invariant_sites) + "}""])
+            command.extend(["-m","GTRGAMMA+ASC_FELS{" + str(invariant_sites) + "}"])
         else:
             command.extend(["-m", self.model + "+ASC_FELS{" + str(invariant_sites) + "}"])
         command.extend(["-p",self.seed])
